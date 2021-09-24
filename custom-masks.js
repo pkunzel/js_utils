05fbce0d4a9a4b58bd245a469769a8a3102eb908
pkunzel <paulo.kunzel@gmail.com>
2021-09-24 16:16:01 -0300

    Created Class with static methods for the masking Created Class for the event logic

---
+++ custom-masks.js
@@ -0,0 +1,108 @@
+/**
+ * @classdesc Contains a list of ready to use static masking functions
+ */
+class ApplyMask {
+	/**
+	 * @description Applies the CPF mask to a string. 000.000.000-00
+	 * @param {String} unformatedValue An unformated string containing the full or partial CPF
+	 * @returns The formatted CPF
+	 */
+	toCPF = (unformatedValue) => {
+		return unformatedValue
+			.replace(/\D/g, "")
+			.replace(/(\d{3})(\d)/, "$1.$2")
+			.replace(/(\d{3})(\d)/, "$1.$2")
+			.replace(/(\d{3})(\d{1,2})/, "$1-$2")
+			.replace(/(-\d{2})\d+?$/, "$1");
+	};
+
+	/**
+	 * @description Applies the CNPJ mask to a string. 00.000.000/0000-00
+	 * @param {String} unformatedValue An unformated string containing the full or partial CNPJ
+	 * @returns The formatted CNPJ
+	 */
+	toCNPJ = (unformatedValue) => {
+		return unformatedValue
+			.replace(/\D/g, "")
+			.replace(/(\d{2})(\d)/, "$1.$2")
+			.replace(/(\d{3})(\d)/, "$1.$2")
+			.replace(/(\d{3})(\d)/, "$1/$2")
+			.replace(/(\d{4})(\d{1,2})/, "$1-$2")
+			.replace(/(-\d{2})\d+?$/, "$1");
+	};
+
+	/**
+	 * @description Applies the Phone mask to a string. (00)00000-0000
+	 * @param {String} unformatedValue An unformated string containing the full or partial CPF
+	 * @returns The formatted CPF
+	 */
+	toPhone = (unformatedValue) => {
+		return unformatedValue
+			.replace(/\D/g, "")
+			.replace(/(\d{2})(\d)/, "($1) $2")
+			.replace(/(\d{5})(\d{4})(\d)/, "$1-$2");
+	};
+
+	/**
+	 * @description Applies the Phone mask to a string. 00000-000
+	 * @param {String} unformatedValue An unformated string containing the full or partial CPF
+	 * @returns The formatted CPF
+	 */
+	toCEP = (unformatedValue) => {
+		return unformatedValue.replace(/\D/g, "").replace(/^(\d{5})(\d{3})+?$/, "$1-$2");
+	};
+
+	/**
+	 * @description Applies the Phone mask to a string. DD/MM/YYYY
+	 * @param {String} unformatedValue An unformated string containing the full or partial Date
+	 * @returns The formatted Date
+	 */
+	toDate = (unformatedValue) => {
+		return unformatedValue
+			.replace(/\D/g, "")
+			.replace(/(\d{2})(\d)/, "$1/$2")
+			.replace(/(\d{2})(\d)/, "$1/$2")
+			.replace(/(\d{4})(\d)/, "$1");
+	};
+
+	/**
+	 * @description Filters out all but the letters
+	 * @param {String} unformatedValue An unformated string
+	 * @returns Only the letters contained in the unformatted string
+	 */
+	toOnlyLetters = (unformatedValue) => {
+		return unformatedValue.replace(/[0-9!@#¨$%^&*)(+=._-]+/g, "");
+	};
+
+	/**
+	 * @description Filters out all but the numbers
+	 * @param {String} unformatedValue An unformated string
+	 * @returns Only the numbers contained in the unformatted string
+	 */
+	toOnlyNumbers = (unformatedValue) => {
+		return unformatedValue.replace(/\D/g, "");
+	};
+}
+
+const maskingActions = {
+	cpf: (element) => applyMaskingEvents(element, ApplyMask.toCPF),
+	cnpj: (element) => applyMaskingEvents(element, ApplyMask.toCNPJ),
+};
+
+function applyMaskingEvents(element, maskFunction) {
+	const elementStringContainer = element.value ? 'value' : 'textcontent';
+	setElementWithMask(elementStringContainer);
+	element.addEventListener("change", setElementWithMask);
+	element.addEventListener("keypress", setElementWithMask);
+
+	function setElementWithMask(elementStringContainer) {
+		element[elementStringContainer] = maskFunction(element[elementStringContainer]);
+	}
+}
+
+const elementsToMask = Array.from(document.querySelectorAll("[data-custom-mask]"));
+
+elementsToMask.forEach((element) => {
+	const customMaskType = element.getAttribute('data-custom-mask');
+	maskingActions[customMaskType](element);
+});
